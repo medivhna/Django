a0a2e613906811a924426ebb689e4c8e07b0b84d
Medivhna <teljerry@live.cn>
2014-05-03 23:22:13 +0800

    mysite v2.2

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,13 +1,19 @@
 from django.http import Http404, HttpResponse
-from django.template.loader import get_template 
-from django.template import Context
+from django.shortcuts import render_to_response
 import datetime
 
-def current_datetime(request):
+# Older Method
+#from django.template.loader import get_template 
+#from django.template import Context
+#def current_datetime(request):
+#	now = datetime.datetime.now()
+#	t = get_template('current_datetime.html')
+#	html = t.render(Context({'current_date': now}))
+#	return HttpResponse(html)
+
+def current_date(request):
 	now = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	html = t.render(Context({'current_date': now}))
-	return HttpResponse(html)
+	return render_to_response('current_datetime.html', {'current_date': now}})
 def hours_ahead(request, offset):
 	try:
 		offset = int(offset)
