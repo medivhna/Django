e56a1c6b4495efeaf8f386496aede885798d28f5
Medivhna <teljerry@live.cn>
2014-05-03 23:09:06 +0800

    mysite v2.1.1

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -7,6 +7,7 @@ def current_datetime(request):
 	now = datetime.datetime.now()
 	t = get_template('current_datetime.html')
 	html = t.render(Context({'current_date': now}))
+	return HttpResponse(html)
 def hours_ahead(request, offset):
 	try:
 		offset = int(offset)
